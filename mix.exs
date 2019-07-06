577975066abae3ac6e4ae745b28ddb2b90b9b83a
Robin Hilliard <robin@rocketboots.com>
2019-07-06 18:06:32 +1000

    Added dialyxir and ex_doc version 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,30 +3,24 @@ defmodule Mavlink.Mixfile do
 
   def project do
     [app: :mavlink,
-     version: "0.1.0",
-     elixir: "~> 1.3",
+     version: "0.1.1",
+     elixir: "~> 1.9",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+      dialyzer: [plt_add_apps: [:mix, :xmerl]]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+
   defp deps do
-    []
+    [
+      {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.20.2", only: [:dev], runtime: false}
+    ]
   end
 end
