55fa06b04a494773546b836914ac98f40a1ffc4b
Robin Hilliard <robin@rocketboots.com>
2019-07-12 21:50:18 +1000

    CRC_EXTRA working! Added some tests confirming pymavlink results matched

--- test/mavlink_utils_test.exs
+++ test/mavlink_utils_test.exs
@@ -11,10 +11,41 @@ defmodule Mavlink.Test.Utils do
   end
   
   test "x25 simple list" do
-    assert x25_crc([1,2,3]) == 38785
+    assert x25_crc([1,2,3]) == 25284 # Checked against mavcrc.x25crc.accumulate()
   end
   
-   test "x25 simple binary" do
-    assert x25_crc("123") == 34701
+  test "x25 simple binary" do
+    assert x25_crc("123") == 25419 # Checked against mavcrc.x25crc.accumulate_str()
+  end
+  
+  test "x25 heartbeat seed" do
+    assert x25_crc("HEARTBEAT ")
+    |> x25_crc("uint32_t ")
+    |> x25_crc("custom_mode ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("type ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("autopilot ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("base_mode ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("system_status ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("mavlink_version ")
+    |> eight_bit_checksum() == 50 # Checked against mavgen
+  end
+  
+  test "x25 change operator control seed" do
+    assert x25_crc("CHANGE_OPERATOR_CONTROL ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("target_system ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("control_request ")
+    |> x25_crc("uint8_t ")
+    |> x25_crc("version ")
+    |> x25_crc("char ")
+    |> x25_crc("passkey ")
+    |> x25_crc([25])
+    |> eight_bit_checksum() == 217 # Checked against mavgen
   end
 end
