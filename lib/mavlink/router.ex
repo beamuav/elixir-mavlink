b5521a4f1dd93dc33c1f13b1f99ad327e9622963
Robin Hilliard <robin@rocketboots.com>
2019-08-17 17:43:48 +1000

    Documentation Dynamic dialect with warning if not set Pass connection strings to init so that supervisor can restart them Added handle_info using code from listen.exs Modified serial connection to use supervised UART processes, not tested Handle invalid protocol passed to connect Discovered the wonders of put_in(). Very different to Putin. Skeleton of validate_and_process_message(). Will be based on listen.exs but will have to use apply(state.dialect, ...) to call generated module

--- lib/mavlink/router.ex
+++ lib/mavlink/router.ex
@@ -1,6 +1,17 @@
 defmodule MAVLink.Router do
-  @moduledoc false
+  @moduledoc """
+  Connect to serial, udp and tcp ports and listen for, validate and
+  forward MAVLink messages towards their destinations on other connections
+  and/or Elixir processes subscribing to messages.
   
+  The rules for MAVLink packet forwarding are described here:
+  
+    https://mavlink.io/en/guide/routing.html
+  
+  and here:
+  
+    http://ardupilot.org/dev/docs/mavlink-routing-in-ardupilot.html
+  """
   
   use GenServer
   
@@ -15,10 +26,15 @@ defmodule MAVLink.Router do
     GenServer.start_link(
       __MODULE__,
       state,
-      [{:name, :"MAVLink.Router"} | opts])
+      [{:name, __MODULE__} | opts])
   end
   
   
+  @ doc """
+  Not recommended to create connections at runtime. You should set
+  connection strings through config.exs so that the supervisor can
+  re-establish connections if the router crashes.
+  """
   def connect(connection_string) do
     GenServer.call __MODULE__, {:connect, connection_string}
   end
@@ -26,22 +42,122 @@ defmodule MAVLink.Router do
   
   # Callbacks
 
+  # TODO refactor init, lacks elegance...
   
   @impl true
-  def init(state) do
-    {:ok, Map.merge(state, %{connections: %{}, targets: %{}})}
+  def init(state = %{dialect: dialect, connections: []}) do
+    if dialect == nil do
+      IO.puts "WARNING: No MAVLink dialect module specified in config.exs."
+    end
+    {
+      :ok,
+      Map.merge(
+        state,
+        %{
+          conn_details: %{},      # serial or network connection
+          targets: %{},           # systems and components
+          subscriptions: %{}      # local PIDs
+        }
+      )
+    }
   end
-
+  
+  def init(state = %{dialect: dialect, connections: [_ | _]}) do
+    if dialect == nil do
+      IO.puts "WARNING: No MAVLink dialect module specified in config.exs."
+    end
+    {
+      :ok,
+      state.connections |> Enum.reduce(
+        Map.merge(
+          state,
+          %{
+            conn_details: %{},      # serial or network connection
+            targets: %{},           # systems and components
+            subscriptions: %{}      # local PIDs
+          }
+        ),
+        fn connection_string, acc_state ->
+          String.split(connection_string, [":", ","]) |> do_connect(acc_state)
+        end
+      )
+    }
+  end
+  
   
   @impl true
   def handle_call({:connect, connection_string}, _from, state) do
-    String.split(connection_string, ":,") |> do_connect(state)
+    String.split(connection_string, [":", ","]) |> do_connect(state)
   end
 
+  
   @impl true
   def handle_cast(_msg, state) do
     {:noreply, state}
   end
+  
+  
+  @doc """
+  Respond to incoming messages from connections
+  """
+  @impl true
+  def handle_info({:udp, _sock, addr, port, raw=
+    <<0xfe,
+      payload_length::unsigned-integer-size(8),
+      sequence_number::unsigned-integer-size(8),
+      system_id::unsigned-integer-size(8),
+      component_id::unsigned-integer-size(8),
+      message_id::unsigned-integer-size(8),
+      payload::binary-size(payload_length),
+      checksum::little-unsigned-integer-size(16)>>}, state) do
+
+    {:noreply, validate_and_process_message(
+        1,
+        {:udp, addr, port},
+        raw,
+        payload_length,
+        sequence_number,
+        system_id,
+        component_id,
+        message_id,
+        payload,
+        checksum,
+        state)}
+  end
+  
+  def handle_info({:udp, _sock, addr, port, raw=
+    <<0xfd,
+      payload_length::unsigned-integer-size(8),
+      0::unsigned-integer-size(8),   # TODO Rejecting all incompatible flags for now
+      _compatible_flags::unsigned-integer-size(8),
+      sequence_number::unsigned-integer-size(8),
+      system_id::unsigned-integer-size(8),
+      component_id::unsigned-integer-size(8),
+      message_id::little-unsigned-integer-size(24),
+      payload::binary-size(payload_length),
+      checksum::little-unsigned-integer-size(16)>>}, state) do
+
+    {:noreply, validate_and_process_message(
+        2,
+        {:udp, addr, port},
+        raw,
+        payload_length,
+        sequence_number,
+        system_id,
+        component_id,
+        message_id,
+        payload,
+        checksum,
+        state)}
+  end
+  
+  def handle_info({:udp, _sock, _addr, _port, _}, state) do
+    IO.puts("Bad Packet")
+    # TODO update statistics, or only with sequence numbers?
+    {:noreply, state}
+  end
+  
+  # TODO TCP and serial (will need to consume up to marker in buffer)
   
   
   # Helpers
@@ -51,20 +167,32 @@ defmodule MAVLink.Router do
     do_connect(["serial", port, "9600"], state)
   end
   
-  defp do_connect(["serial", port, baud], state) do
-    case {File.exists?(port), parse_positive_integer(baud)} do
+  defp do_connect(["serial", _, _], state = %{uarts: []}) do
+    {:reply, {:error, :no_free_uarts}, state}
+  end
+  
+  defp do_connect(["serial", port, baud], state = %{uarts: [next_free_uart | _free_uarts]}) do
+    attached_ports = UART.enumerate()
+    case {Map.has_key?(attached_ports, port), parse_positive_integer(baud)} do
       {false, _} ->
-        {:reply, {:error, :port_doesnt_exist}, state}
+        {:reply, {:error, :port_not_attached}, state}
       {_, :error} ->
         {:reply, {:error, :invalid_baud_rate}, state}
-      {true, b} ->
-        :ok = UART.open(UART, port, speed: b, active: true)
-        {:reply,
-          :ok,
-          %{state | connections: %{
-            state.connections | {:serial, port} => <<>>}
-          }
-        }
+      {true, parsed_baud} ->
+        case UART.open(next_free_uart, port, speed: parsed_baud, active: true) do
+          :ok ->
+            {
+              :reply,
+              :ok,
+              state.conn_details |> Map.put_new(
+                {:serial, port},
+                {next_free_uart, <<>>})
+            }
+
+            
+          error = {:error, _} ->
+            {:reply, error, state}
+        end
     end
   end
   
@@ -75,36 +203,43 @@ defmodule MAVLink.Router do
       {_, :error} ->
         {:reply, {:error, :invalid_port}, state}
       {ip, p} ->
-        do_connect(protocol, ip, p, state)
+        do_connect_network(protocol, ip, p, state)
     end
   end
   
-  defp do_connect("udpin", address, port, state) do
-    {:ok, socket} = :gen_udp.open(port, [:binary, ip: address, active: :true])
-    {:reply,
-      :ok,
-      %{state | connections: %{
-        state.connections | {:udpin, address, port} => {socket, <<>>}}
-       }
-    }
-  end
-  
-  defp do_connect("udpout", address, port, state) do
-    {:ok, socket} = :gen_udp.open(port, [:binary, ip: address, active: :true])
-    {:reply,
-      :ok,
-      %{state | connections: %{
-        state.connections | {:udpout, address, port} => {socket, <<>>}}
-       }
-    }
-  end
-  
-  defp do_connect("tcp", _address, _port, state) do
-    {:reply, "tcp not implemented", state}
-  end
-  
-  defp do_connect(_, _, _, state) do
+  defp do_connect(_, state) do
     {:reply, {:error, :invalid_protocol}, state}
+  end
+  
+  
+  defp do_connect_network("udp", address, port, state) do
+    {:ok, socket} = :gen_udp.open(port, [:binary, ip: address, active: :true])
+    {:reply,
+      :ok,
+      state |> put_in([:conn_details, {:udp, address, port}], socket)
+    }
+  end
+  
+  defp do_connect_network("tcp", _address, _port, state) do
+    {:reply, "tcp not implemented", state} # TODO
+  end
+  
+  
+  defp validate_and_process_message(
+        version,
+        conn_key,
+        raw,
+        payload_length,
+        sequence_number,
+        system_id,
+        component_id,
+        message_id,
+        payload,
+        checksum,
+        state) do
+    IO.puts "#{sequence_number}: Mavlink #{version} message #{message_id}"
+    # TODO Up to here
+    state
   end
   
 end
