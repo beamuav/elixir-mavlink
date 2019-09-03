2172b952b43ac2fb19c3ef621328de4caaf29863
Robin Hilliard <robin@rocketboots.com>
2019-09-03 22:43:22 +1000

    This is what I'm using at the moment for testing in iex, shouldn't be packaged with library

--- .iex.exs
+++ .iex.exs
@@ -1,2 +1,10 @@
 :observer.start
 alias MAVLink.Router, as: MAV
+vfr_hud = %APM.Message.VfrHud{
+  airspeed: 0.0,
+  alt: 43.43000030517578,
+  climb: -0.6640441417694092,
+  groundspeed: 0.5645657181739807,
+  heading: 306,
+  throttle: 0
+}
