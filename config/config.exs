f336d1ed56f7ba0d3cd7ebcfaab9362d8d49ca67
Robin Hilliard <robin@rocketboots.com>
2019-10-13 21:43:56 +1100

    Remove loopback udp

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :mavlink, dialect: APM
-config :mavlink, connections: ["udp:192.168.0.23:14550", "udp:127.0.0.1:14550"]
+config :mavlink, connections: ["udp:192.168.0.14:14550"]
 config :logger, level: :info
