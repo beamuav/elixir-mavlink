32c01cc0fb82b10dc39ad731b386af348d98336e
Robin Hilliard <robin@rocketboots.com>
2019-10-14 20:25:00 +1100

    Don't need to implement handle_info() for UDP Out Deleted unrequired import

--- lib/mavlink/udp_out_connection.ex
+++ lib/mavlink/udp_out_connection.ex
@@ -5,7 +5,6 @@ defmodule MAVLink.UDPOutConnection do
   
   require Logger
   alias MAVLink.Frame
-  import MAVLink.Frame, only: [unpack_frame: 1, validate_and_unpack: 2]
   
   
   defstruct [
@@ -16,30 +15,6 @@ defmodule MAVLink.UDPOutConnection do
                address: MAVLink.Types.net_address,
                port: MAVLink.Types.net_port,
                socket: pid}
-             
-             
-  def handle_info({:udp, socket, source_addr, source_port, raw}, state) do
-    receiving_connection = struct(MAVLink.UDPOutConnection,
-                          socket: socket, address: source_addr, port: source_port)
-    case unpack_frame(raw) do
-      {received_frame, _} -> # UDP sends frame per packet, so ignore rest
-        case validate_and_unpack(received_frame, state.dialect) do
-          {:ok, valid_frame} ->
-            {:ok, receiving_connection, valid_frame, state}
-          :unknown_message ->
-            # We re-broadcast valid frames with unknown messages
-            {:ok, receiving_connection, received_frame, state}
-          reason ->
-              Logger.warn(
-                "UDP MAVLink frame received from " <>
-                "#{Enum.join(Tuple.to_list(source_addr), ".")}:#{source_port} failed: #{Atom.to_string(reason)}")
-              {:error, state}
-        end
-      _ ->
-        # Noise or malformed frame
-        {:error, state}
-    end
-  end
   
   
   def connect(["udp", address, port], state=%MAVLink.Router{connections: connections}) do
