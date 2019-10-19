d46175e653a642b103699277f09e11bfa4060270
Robin Hilliard <robin@rocketboots.com>
2019-10-19 22:06:13 +1100

    Less verbose, as we're getting ~ 250 messages per second

--- scripts/echo.exs
+++ scripts/echo.exs
@@ -1,16 +1,16 @@
 alias MAVLink.Router, as: MAV
 
 defmodule Echo do
-  def run() do
+  def run(count \\ 1) do
     receive do
       msg ->
-        IO.inspect msg
+        IO.puts "#{System.system_time(:second)} #{count}: #{Atom.to_string(msg.__struct__)}"
         if :random.uniform < 0.2 do
           IO.inspect MAV.pack_and_send(msg)
           IO.puts "****** The next #{Atom.to_string(msg.__struct__)} message is a round-trip repeat of above message ******"
         end
     end
-    run()
+    run(count + 1)
   end
 end
 
