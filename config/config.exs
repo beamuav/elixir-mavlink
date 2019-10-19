e3e3e1277418c4154fe6192b3a5f41185e13bfaf
Robin Hilliard <robin@rocketboots.com>
2019-10-19 21:53:39 +1100

    I discover the very simple reason I was not getting both connections at once.

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,5 @@
 use Mix.Config
 
 config :mavlink, dialect: APM
-config :mavlink, connections: ["udpin:192.168.0.14:14550"] # APM
-config :mavlink, connections: ["tcpout:192.168.0.14:5760"] # SITL
-config :logger, level: :debug
+config :mavlink, connections: ["udpin:192.168.0.14:14550", "tcpout:192.168.0.14:5760"] # APM, SITL
+config :logger, level: :warn
