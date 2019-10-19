5c3be9ece96709a4b4890dfd56a8dbb5823d1223
Robin Hilliard <robin@rocketboots.com>
2019-10-19 16:17:09 +1100

    handle_info() creates connection first time it is seen success and errors include updated connection state instead of entire state Add address and port to connection key because UDP "in" can have multiple clients

--- lib/mavlink/udp_in_connection.ex
+++ lib/mavlink/udp_in_connection.ex
@@ -16,28 +16,35 @@ defmodule MAVLink.UDPInConnection do
                address: MAVLink.Types.net_address,
                port: MAVLink.Types.net_port,
                socket: pid}
-             
-             
-  def handle_info({:udp, socket, source_addr, source_port, raw}, state) do
-    receiving_connection = struct(MAVLink.UDPInConnection,
-                          socket: socket, address: source_addr, port: source_port)
+  
+  # Create connection if this is the first time we've received on it
+  def handle_info({:udp, socket, source_addr, source_port, raw}, nil, dialect) do
+    handle_info(
+      {:udp, socket, source_addr, source_port, raw},
+      %MAVLink.UDPInConnection{address: source_addr, port: source_port, socket: socket},
+      dialect)
+  end
+
+  def handle_info({:udp, socket, source_addr, source_port, raw}, receiving_connection, dialect) do
     case binary_to_frame_and_tail(raw) do
       :not_a_frame ->
         # Noise or malformed frame
         Logger.warn("UDPInConnection.handle_info: Not a frame #{inspect(raw)}")
-        {:error, state}
+        {:error, :not_a_frame, {socket, source_addr, source_port}, receiving_connection}
       {received_frame, _rest} -> # UDP sends frame per packet, so ignore rest
-        case validate_and_unpack(received_frame, state.dialect) do
+        case validate_and_unpack(received_frame, dialect) do
           {:ok, valid_frame} ->
-            {:ok, receiving_connection, valid_frame, state}
+            # Include address and port in connection key because multiple
+            # clients can connect to a UDP "in" port.
+            {:ok, {socket, source_addr, source_port}, receiving_connection, valid_frame}
           :unknown_message ->
             # We re-broadcast valid frames with unknown messages
-            {:ok, receiving_connection, received_frame, state}
+            {:ok, {socket, source_addr, source_port}, receiving_connection, received_frame}
           reason ->
               Logger.warn(
                 "UDPInConnection.handle_info: frame received from " <>
                 "#{Enum.join(Tuple.to_list(source_addr), ".")}:#{source_port} failed: #{Atom.to_string(reason)}")
-              {:error, state}
+              {:error, reason, {socket, source_addr, source_port}, receiving_connection}
         end
     end
   end
@@ -58,18 +65,14 @@ defmodule MAVLink.UDPInConnection do
   
   def forward(%MAVLink.UDPInConnection{
       socket: socket, address: address, port: port},
-      %Frame{version: 1, mavlink_1_raw: packet},
-      state) do
+      %Frame{version: 1, mavlink_1_raw: packet}) do
     :gen_udp.send(socket, address, port, packet)
-    {:noreply, state}
   end
   
   def forward(%MAVLink.UDPInConnection{
       socket: socket, address: address, port: port},
-      %Frame{version: 2, mavlink_2_raw: packet},
-      state) do
+      %Frame{version: 2, mavlink_2_raw: packet}) do
     :gen_udp.send(socket, address, port, packet)
-    {:noreply, state}
   end
 
 end
