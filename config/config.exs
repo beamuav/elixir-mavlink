130402f3e57d3002803d44b1ea2e77c5280fe271
Robin Hilliard <robin@rocketboots.com>
2019-10-20 22:15:07 +1100

    Added QGroundControl connection, HAS to be loopback otherwise QGC will assume that and you'll get no packets back from it, might be a QGC bug

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :mavlink, dialect: APM
-config :mavlink, connections: ["udpin:192.168.0.14:14550", "tcpout:192.168.0.14:5760"] # APM, SITL
-config :logger, level: :warn
+config :mavlink, connections: ["udpin:192.168.0.14:15550", "udpout:127.0.0.1:14550", "tcpout:127.0.0.1:5760"] # APM, QGC (HAS to be loopback), SITL
+config :logger, level: :error
