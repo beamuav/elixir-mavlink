ec1514aa164069683c3d1fa4992d22fae682aea7
Robin Hilliard <robin@rocketboots.com>
2019-10-20 22:13:38 +1100

    Currently set up to look at targeted messages between QGC and SITL

--- scripts/echo.exs
+++ scripts/echo.exs
@@ -4,17 +4,19 @@ defmodule Echo do
   def run(count \\ 1) do
     receive do
       msg ->
-        IO.puts "#{System.system_time(:second)} #{count}: #{Atom.to_string(msg.__struct__)}"
-        if :random.uniform < 0.2 do
-          IO.inspect MAV.pack_and_send(msg)
-          IO.puts "****** The next #{Atom.to_string(msg.__struct__)} message is a round-trip repeat of above message ******"
-        end
+        IO.puts "#{msg.source_system} -> #{msg.target_system}: #{Atom.to_string(msg.message.__struct__)}"
+#        IO.inspect msg
+#        if :random.uniform < 0.2 do
+#          IO.puts "****** The next #{Atom.to_string(msg.__struct__)} message is a round-trip repeat of above message ******"
+#          IO.inspect MAV.pack_and_send(msg)
+#        end
     end
     run(count + 1)
   end
 end
 
 :observer.start
-MAV.subscribe #message: APM.Message.RcChannelsRaw
+MAV.subscribe as_frame: true, target_system: 250
+MAV.subscribe as_frame: true, target_system: 1
 Echo.run()
 
