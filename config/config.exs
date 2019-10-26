b440eb1a0f5a58a21d3f46129ae9b85624e2211d
Robin Hilliard <robin@rocketboots.com>
2019-10-26 19:53:01 +1100

    Single line mavlink config

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
-config :mavlink, dialect: APM
-config :mavlink, connections: ["udpin:192.168.0.14:15550", "udpout:127.0.0.1:14550", "tcpout:127.0.0.1:5760"] # APM, QGC (HAS to be loopback), SITL
-config :logger, level: :error
+config :mavlink, dialect: APM, connections: ["udpin:192.168.0.101:15550", "udpout:127.0.0.1:14550", "tcpout:127.0.0.1:5760"] # APM, QGC (HAS to be loopback), SITL
+config :logger, level: :warn
+
