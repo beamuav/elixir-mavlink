34d2f109b18dcc3292d989ce43083eb4e22e03e3
Robin Hilliard <robin@rocketboots.com>
2019-11-05 07:35:02 +1100

    Bump 0.8.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MAVLink.Mixfile do
   def project do
     [
       app: :mavlink,
-      version: "0.7.0",
+      version: "0.8.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
